dff808f59ad6963d988e11d789d9ebff511e9181
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-03-27 13:38:32 +0000

    error

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import { router } from "./routes/allroutes.js";
 
 dotenv.config()
 const app = express();
 
 
-app.use(express.json())
+app.use(express.json());
 
 
 const PORT = process.env.PORT ||8000
@@ -19,7 +20,7 @@ mongoose.connect(mongoURI).then(()=>{
 }).catch((error)=> console.log(error))
 
 
-
+app.use(router);
 
 app.listen(PORT, () => {
     console.log("onyx portfolio api is running");
