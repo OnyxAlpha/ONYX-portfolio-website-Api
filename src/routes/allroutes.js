c5bbf2b4e26534af69584bbaa4b254bcb2dcfc99
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-03-27 18:04:48 +0000

    edit achievement model

--- src/routes/allroutes.js
+++ src/routes/allroutes.js
@@ -2,8 +2,22 @@ import { Router } from "express";
 import { addBlogs, deleteOneBlog, getAllBlogs, getOneBlog, upgradeBlog } from "../controllers/blogs_controllers.js";
 import { addAchievements, deleteOneAchievement, editAchievement, getAllAchievements, getOneAchievements } from "../controllers/achievements_controllers.js";
 import { addExperience, deleteExperience, editExperience, getAllExperiences, getOneExperience } from "../controllers/experiences_controllers.js";
+import dotenv from "dotenv";
+import { MulterSaveFilesOrgStorage } from "multer-savefilesorg-storage";
+import multer from "multer";
 
 
+
+dotenv.config()
+
+const upload = multer({
+    storage:MulterSaveFilesOrgStorage({
+        serverPath: `https://savefiles.org/api/v1/uploads`,
+        apiAccessToken: process.env.SAVEFILESORG_API_KEY,
+        relativePath: '/portfolio/images/*'
+    }),
+    preservePath: true
+});
 
 
 export const router =Router();
