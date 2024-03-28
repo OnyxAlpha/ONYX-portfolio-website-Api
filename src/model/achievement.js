9e2b5bde3be762c4dabae2b0e095e41942dfd372
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-03-28 13:51:32 +0000

    title

--- src/model/achievement.js
+++ src/model/achievement.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const schema = mongoose.Schema;
 
 const achievementSchema=new schema({
+    title:{type: String ,required: true },
     description:{type: String ,required: true },
     image:{type:String},
     date: { type: Date },
